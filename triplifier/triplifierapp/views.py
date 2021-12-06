98df80abde89e3a9a9c5bf246f621834db2ea625
Charles <charles.laurioz@gmail.com>
2021-12-06 18:07:01 +0100

    maj de certains bugs mais pas tous

--- triplifier/triplifierapp/views.py
+++ triplifier/triplifierapp/views.py
@@ -1,9 +1,12 @@
+from typing import cast
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .forms import uploadCsvForm, convertForm
 from .models import csvModel, ttlModel
 from utils.triplificator import Triplificator
 from django.core.files import File
+from django.contrib import messages
+import os
 
 # Create your views here.
 
@@ -19,9 +22,14 @@ def triplifier(request):
                 else:
                     document.csvFile.name = str(request.FILES["csvFile"])
                     document.csvFileName = str(request.FILES["csvFile"]).replace(".csv","")
+
+                if (document.csvFileName in list(csvModel.objects.all().values_list('csvFileName',flat=True))):
+                    messages.add_message(request, messages.ERROR, 'CSV name already exists !')
+                    return redirect('/')
+                messages.add_message(request, messages.INFO, 'CSV added !')
                 document.save()
                 #once saved, the attributes for csvFile change, name and url adapt to the path
-                #where it is located (tpData/csv/...) and adapt if file in duplicate (ex test1_ze24GEZdz.csv)
+                #where it is located (tpData/csv/...) and adapt if file is in duplicate (ex test1_ze24GEZdz.csv)
                 return redirect('/')
 
         elif "convertBut" in request.POST:
@@ -29,21 +37,25 @@ def triplifier(request):
             form2 = convertForm(request.POST)
             if form2.is_valid():
                 print("POST REQUEST")
-                print(form2.cleaned_data.get("separator"))
+                print(form2.cleaned_data.get("title"))
                 print(form2.cleaned_data.get("titleRow"))
-                print(request.POST.get("titleRow"))
+                print(form2.cleaned_data.get("fileToConvert"))
+                print(type(form2.cleaned_data.get("newFileName")))
+                print(str(form2.cleaned_data.get("newFileName")))
                 print(request.POST.get("newFileName"))
                 trpObj = Triplificator("tpData/csv/"+str(form2.cleaned_data.get("fileToConvert"))+".csv"
                 , form2.cleaned_data.get("titleRow"), form2.cleaned_data.get("firstDataRow")
                 , form2.cleaned_data.get("lastDataRow"), form2.cleaned_data.get("separator")
-                , form2.cleaned_data.get("prefixData"), form2.cleaned_data.get("predicatData"))
-                trpObj.writeFile("tpData/ttl/"+request.POST.get("newFileName")+".ttl")
+                , form2.cleaned_data.get("prefixData"), form2.cleaned_data.get("predicatData"), form2.cleaned_data.get("title"))
+
+                trpObj.writeFile("tpData/ttl/"+str(form2.cleaned_data.get("newFileName"))+".ttl")
 
                 ttlObj = ttlModel()
-                ttlObj.ttlFileName = request.POST.get("newFileName")
-                
-                local_file = open("tpData/ttl/"+request.POST.get("newFileName")+".ttl")
-                ttlObj.ttlFile.save(request.POST.get("newFileName")+".ttl", File(local_file)) #surement ca qui enregistre en double
+                ttlObj.ttlFileName = str(form2.cleaned_data.get("newFileName"))
+
+                local_file = open("tpData/ttl/"+str(form2.cleaned_data.get("newFileName"))+".ttl", 'rb')
+                ttlObj.ttlFile.save(str(form2.cleaned_data.get("newFileName"))+".ttl", File(local_file)) #getting duplicated file in cd
+                os.remove(ttlObj.ttlFileName+".ttl") #so we delete this file
                 local_file.close()
 
                 ttlObj.save()
@@ -58,15 +70,14 @@ def triplifier(request):
 
     return render(request, 'triplifier.html', {"form":form1, "csvFiles":csvObjs, "ttlFiles":ttlObjs, "formConvert":form2})
 
-
 def exportCSV(request, filename):
-    content = open("tpData/csv/"+filename, "r")
+    content = open("tpData/csv/"+filename, "r", encoding="utf-8")
     response = HttpResponse(content, content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename={0}'.format(filename)
     return response
 
 def exportTTL(request, filename):
-    content = open("tpData/ttl/"+filename+".ttl", "r")
+    content = open("tpData/ttl/"+filename+".ttl", "r", encoding="utf-8")
     response = HttpResponse(content, content_type='application/force-download')
     response['Content-Disposition'] = 'attachment; filename={0}'.format(filename)+".ttl"
     return response
@@ -75,13 +86,19 @@ def exportTTL(request, filename):
 def delete_csv_file(request, csvName):
     if request.method == "POST":
         csvFile = csvModel.objects.get(csvFileName=csvName)
-        os.remove("tpData/csv/"+str(csvFile.csvFileName)+".csv") #so we delete this file
-        csvFile.delete()
+        try:
+            os.remove("tpData/csv/"+str(csvFile.csvFileName)+".csv") #so we delete this file
+            csvFile.delete()
+        except:
+            print("cannot delete the file as it is still open")
     return redirect('/')
 
 def delete_ttl_file(request, ttlName):
     if request.method == "POST":
         ttlFile = ttlModel.objects.get(ttlFileName=ttlName)
-        os.remove("tpData/ttl/"+str(ttlFile.ttlFileName)+".ttl") #so we delete this file
-        ttlFile.delete()
+        try:   
+            os.remove("tpData/ttl/"+str(ttlFile.ttlFileName)+".ttl") #so we delete this file
+            ttlFile.delete()
+        except:
+            print("cannot delete the file as it is still open")
     return redirect('/')
